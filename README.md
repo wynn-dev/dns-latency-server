8cf5dbf163741b6a96d07ad17dd7d00135854fd5
wynn <209275660@qq.com>
2022-10-18 14:54:58 +0100

    Update install.sh

---
+++ README.md
@@ -0,0 +1,1 @@
+wget https://raw.githubusercontent.com/wynn-dev/dns-latency-server/main/install.sh; bash install.sh
--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
-git clone
+cd ~
+git clone https://github.com/wynn-dev/dns-latency-server.git
 cd dns-latency-server
 npm install
 npm install pm2 -g
