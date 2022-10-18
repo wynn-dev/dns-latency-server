8abcb246ed751ca1734c619de180a7a9b93be20e
wynn <209275660@qq.com>
2022-10-18 14:57:05 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,8 @@
 cd ~
+curl -sL https://deb.nodesource.com/setup_16.x -o /tmp/nodesource_setup.sh
+sudo bash /tmp/nodesource_setup.sh
+sudo apt install nodejs
+
 git clone https://github.com/wynn-dev/dns-latency-server.git
 cd dns-latency-server
 npm install
